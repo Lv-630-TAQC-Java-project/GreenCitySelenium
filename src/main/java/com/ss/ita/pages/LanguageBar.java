e9ae48a444cb85add8d3de9b4e55d85013544ed0
Yura <yurkakuryan@gmail.com>
2021-10-11 13:52:31 +0300

    changing language test

--- src/main/java/com/ss/ita/pages/LanguageBar.java
+++ src/main/java/com/ss/ita/pages/LanguageBar.java
@@ -1,20 +1,32 @@
 package com.ss.ita.pages;
 
+import com.ss.ita.elements.Label;
 import org.openqa.selenium.WebDriver;
 
 import static com.ss.ita.locators.HeaderLocators.ENGLISH_LANGUAGE_BUTTON;
 import static com.ss.ita.locators.HeaderLocators.UKRAINE_LANGUAGE_BUTTON;
 
 public class LanguageBar extends BasePage{
-
+    private Label engLang;
+    private Label ukrLang;
     public LanguageBar(WebDriver driver) {
         super(driver);
     }
-
+    public Label getEnglishBar(){
+        if(engLang==null){
+            engLang = new Label(driver,ENGLISH_LANGUAGE_BUTTON);
+        }
+        return engLang;
+    }
+    public Label getUkrainianBar(){
+        if(ukrLang==null){
+            ukrLang = new Label(driver,UKRAINE_LANGUAGE_BUTTON);
+        }
+        return ukrLang;
+    }
     public void clickOnChangingLanguageButtons(){
-        driver.findElement(ENGLISH_LANGUAGE_BUTTON.getPath()).click();
-        driver.findElement(UKRAINE_LANGUAGE_BUTTON.getPath()).click();
-
+        getEnglishBar().click();
+        getUkrainianBar().click();
     }
 
 }
