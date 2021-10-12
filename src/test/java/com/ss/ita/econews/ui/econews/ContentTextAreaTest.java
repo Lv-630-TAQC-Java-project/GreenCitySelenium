5077b98f19cfd6dbc16d320702a53f7c38d47c44
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-10-12 22:19:49 +0300

    add textAreaDecoration method

--- src/test/java/com/ss/ita/econews/ui/econews/ContentTextAreaTest.java
+++ src/test/java/com/ss/ita/econews/ui/econews/ContentTextAreaTest.java
@@ -11,59 +11,58 @@ import org.testng.annotations.Test;
 import static com.ss.ita.econews.ui.data.UserSignInData.TARAS_KRASITSKYI;
 import static org.testng.Assert.assertTrue;
 
-
 public class ContentTextAreaTest extends TestRuner {
 
-    private CreateNewsPage createNewsPage;
+	private CreateNewsPage createNewsPage;
 
-    @DataProvider
-    public static Object[][] dpTextAreaDescriptionIsRedTest() {
-        return new Object[][]{{"too short text", "#ff0000"},
-                {"", "#ff0000"},
-                {"almost enogth text)", "#ff0000"}};
-    }
+	@DataProvider
+	public static Object[][] dpTextAreaDecorationIsGreyTest() {
+		return new Object[][] { { "Text whith length more than 20", "#878787" }, { "Text length equals 21", "#878787" },
+				{ "Text length equals20)", "#878787" } };
+	}
 
-    @BeforeClass
-    public void beforeContentTextAreaTest() {
-        createNewsPage = new HomePage(driver)
-                .getHeader()
-                .login(TARAS_KRASITSKYI.getEmail(), TARAS_KRASITSKYI.getPassword())
-                .clickHomePageLink()
-                .clickEcoNewsLink()
-                .clickCreateNewsButton();
-    }
+	@DataProvider
+	public static Object[][] dpTextAreaDescriptionIsRedTest() {
+		return new Object[][] { { "too short text", "#ff0000" }, { "", "#ff0000" },
+				{ "almost enogth text)", "#ff0000" } };
+	}
 
-    @Test(dataProvider = "dpTextAreaDescriptionIsRedTest")
-    public void textAreaDescriptionIsRedTest(String shortText, String expectedTextColor) {
-        createNewsPage = createNewsPage
-                .setTextArea(shortText)
-                .clickTextAreaDescription();
-        String colorTextParagraph = Color.fromString(createNewsPage.getParagraph()
-                        .getColorTextParagraph())
-                .asHex();
+	@BeforeClass
+	public void beforeContentTextAreaTest() {
+		createNewsPage = new HomePage(driver).getHeader()
+				.login(TARAS_KRASITSKYI.getEmail(), TARAS_KRASITSKYI.getPassword()).clickHomePageLink()
+				.clickEcoNewsLink().clickCreateNewsButton();
+	}
 
+	@Test(dataProvider = "dpTextAreaDescriptionIsRedTest")
+	public void textAreaDescriptionIsRedTest(String shortText, String expectedTextColor) {
+		createNewsPage = createNewsPage.setTextArea(shortText).clickTextAreaDescription();
+		String colorTextParagraph = Color.fromString(createNewsPage.getParagraph().getColorTextParagraph()).asHex();
 
-        softAssert.assertEquals(colorTextParagraph, expectedTextColor);
+		softAssert.assertEquals(colorTextParagraph, expectedTextColor);
 
-        softAssert.assertEquals(Color
-                .fromString(createNewsPage
-                        .getTextArea()
-                        .getBorderColor())
-                .asHex(), expectedTextColor);
-        softAssert.assertAll();
-    }
+		softAssert.assertEquals(Color.fromString(createNewsPage.getTextArea().getBorderColor()).asHex(),
+				expectedTextColor);
+		softAssert.assertAll();
+	}
 
-    @Test(dataProvider = "dpTextAreaDescriptionIsRedTest")
-    public void borderAreaDescriptionIsRedTest(String shortText, String expectedTextColor) {
-        createNewsPage = createNewsPage
-                .setTextArea(shortText)
-                .clickTextAreaDescription();
-        assertTrue((Color
-                .fromString(createNewsPage
-                        .getTextArea()
-                        .getBorderColor())
-                .asHex())
-                .equals(expectedTextColor));
+	@Test(dataProvider = "dpTextAreaDecorationIsGreyTest")
+    public void textAreaDecorationIsGreyTest(String text, String expectedTextColor) {
+    	 createNewsPage = createNewsPage
+                 .setTextArea(text)
+                 .clickTextAreaDescription();
+    	 softAssert.assertEquals(Color
+                 .fromString(createNewsPage
+                         .getTextArea()
+                         .getBorderColor())
+                 .asHex(), expectedTextColor);
+    	 softAssert.assertEquals(Color
+                 .fromString(createNewsPage
+                         .getParagraph().getColorTextParagraph())
+                 .asHex(), expectedTextColor);
+    	 
+         softAssert.assertAll();
+    	
     }
 
 }
