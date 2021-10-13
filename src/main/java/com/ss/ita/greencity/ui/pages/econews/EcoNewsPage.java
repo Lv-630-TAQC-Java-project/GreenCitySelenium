fb0dbcd62cf652995bd862bc134ac077e33e5fb0
NazarYakh <nazaryakh@gmail.com>
2021-10-13 18:48:51 +0300

    Not logged user bug

--- src/main/java/com/ss/ita/greencity/ui/pages/econews/EcoNewsPage.java
+++ src/main/java/com/ss/ita/greencity/ui/pages/econews/EcoNewsPage.java
@@ -3,12 +3,19 @@ package com.ss.ita.greencity.ui.pages.econews;
 import com.ss.ita.greencity.ui.elements.Button;
 import com.ss.ita.greencity.ui.elements.Label;
 import com.ss.ita.greencity.ui.locators.CreateNewsPageLocators;
+import com.ss.ita.greencity.ui.locators.EcoNewsLocators;
 import com.ss.ita.greencity.ui.pages.BasePage;
 import com.ss.ita.greencity.ui.pages.CreateNewsPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.lang.model.element.Element;
+
+import java.text.BreakIterator;
 
 import static com.ss.ita.greencity.ui.locators.EcoNewsLocators.*;
 
@@ -22,6 +29,7 @@ public class EcoNewsPage extends BasePage {
     private Label newsTitle;
     private Button newsButton;
     private EcoNewsListComponent news;
+    private EcoNewsPage scroll;
 
 
     public EcoNewsListComponent getNews() {
@@ -108,5 +116,31 @@ public class EcoNewsPage extends BasePage {
     public NewsPage clickNewsButton() {
         getNewsButton().clickButton();
         return new NewsPage(driver);
+    }
+    public EcoNewsPage getScroll() {
+        driver.findElement(FOOTER.getPath());
+        return new EcoNewsPage(driver);
+    }
+    public EcoNewsPage scrollPageToTheBottom() {
+        WebElement scrollPage = (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".list-wrapper")));
+        while (true) {
+            ((JavascriptExecutor) driver)
+                    .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            try {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                WebElement circle = (new WebDriverWait(driver, 5)).until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath("//*[name()='circle' and contains(@cx,'50%')]")));
+            }
+            catch(Throwable breakIterator) {
+                break;
+            }
+            // wait till sircle dissapear
+        }
+        return new EcoNewsPage(driver);
     }
 }
--- src/test/java/com/ss/ita/econews/ui/econews/NewsListTest.java
+++ src/test/java/com/ss/ita/econews/ui/econews/NewsListTest.java
@@ -1,23 +1,39 @@
 package com.ss.ita.econews.ui.econews;
 
 import com.ss.ita.econews.ui.runner.TestRuner;
+import com.ss.ita.greencity.ui.locators.EcoNewsLocators;
 import com.ss.ita.greencity.ui.pages.HomePage;
 import com.ss.ita.greencity.ui.pages.econews.EcoNewsListItemComponent;
 import com.ss.ita.greencity.ui.pages.econews.EcoNewsPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class NewsListTest extends TestRuner {
 
     @Test
     public void testFirstNews() {
-        EcoNewsListItemComponent ecoNewsItem = new HomePage(driver).
+        EcoNewsPage ecoNewsItem = new HomePage(driver).
                 getHeader().
                 clickEcoNewsLink().
-                getNews()
-                .get(0);
-        softAssert.assertEquals(ecoNewsItem.getTitle().getText(), "Test News");
-        softAssert.assertEquals(ecoNewsItem.getContent().getText(), "Description for test news");
-        softAssert.assertAll();
+                scrollPageToTheBottom();
+        int count = driver.findElements(By.cssSelector(".list.gallery-view-active > .gallery-view-li-active")).size();
+        System.out.println("Count: " + count);
+        String total_Items = driver.findElement(
+                By.cssSelector("div[class='main-wrapper'] app-remaining-count div h2")).getText();
+//        System.out.println("total_Items: " + total_Items);
+        String[] splited = total_Items.split(" ");
+        Assert.assertEquals(count, Integer.parseInt(splited[0]));
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        softAssert.assertEquals(ecoNewsItem.getTitle().getText(), "Test News");
+//        softAssert.assertEquals(ecoNewsItem.getContent().getText(), "Description for test news");
+//        softAssert.assertAll();
         }
 }
