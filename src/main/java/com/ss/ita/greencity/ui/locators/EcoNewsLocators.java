39f03d46cd9c54ec611feda3bf40b55a3515fe6c
Yurka Kurian <55287736+NyxLex@users.noreply.github.com>
2021-10-25 14:23:18 +0300

    Update EcoNewsLocators.java

--- src/main/java/com/ss/ita/greencity/ui/locators/EcoNewsLocators.java
+++ src/main/java/com/ss/ita/greencity/ui/locators/EcoNewsLocators.java
@@ -2,7 +2,7 @@ package com.ss.ita.greencity.ui.locators;
 
 import org.openqa.selenium.By;
 
-public enum EcoNewsLocators  implements BaseLocator {
+public enum EcoNewsLocators implements BaseLocator {
     ECO_NEWS_MAIN_HEADER(By.cssSelector("#main-content > div > div:nth-child(1) > div > h1")),
     TEST_NEWS_TITLE(By.xpath("(//h3[contains(text(),'Test News')][1]")),
     FFFFFFFFFF_NEWS_TITLE(By.xpath("//h3[contains(text(),' ffffffffffffffffff ')]")),
