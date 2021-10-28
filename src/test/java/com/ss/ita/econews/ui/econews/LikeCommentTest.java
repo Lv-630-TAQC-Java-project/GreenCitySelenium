63cc71642553c0306f1a7d65f6c85148fd0f6f8d
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-10-28 12:02:36 +0300

    GC-851, add new webElement

--- src/test/java/com/ss/ita/econews/ui/econews/LikeCommentTest.java
+++ src/test/java/com/ss/ita/econews/ui/econews/LikeCommentTest.java
@@ -3,18 +3,11 @@ package com.ss.ita.econews.ui.econews;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import com.ss.ita.econews.ui.runner.TestRuner;
-import com.ss.ita.greencity.ui.pages.CreateNewsPage;
 import com.ss.ita.greencity.ui.pages.HomePage;
-import com.ss.ita.greencity.ui.pages.econews.EcoNewsListComponent;
-import com.ss.ita.greencity.ui.pages.econews.EcoNewsListItemComponent;
-import com.ss.ita.greencity.ui.pages.econews.EcoNewsPage;
-import com.ss.ita.greencity.ui.pages.news.NewsListCommentComponent;
-import com.ss.ita.greencity.ui.pages.news.NewsListComponent;
 import com.ss.ita.greencity.ui.pages.news.NewsPage;
-
 import static com.ss.ita.econews.ui.data.UserSignInData.TEST_ACCOUNT;
 
-import java.util.List;
+
 
 public class LikeCommentTest extends TestRuner {
 private NewsPage singleNews;
@@ -30,26 +23,7 @@ private NewsPage singleNews;
 				.get(0)
 				.click();
 	}
- //	public void createComment() {
-//		NewsListCommentComponent ecoNews = new HomePage(driver).getHeader()
-//				.login(TEST_ACCOUNT.getEmail(), TEST_ACCOUNT.getPassword())
-//				.clickHomePageLink()
-//				.clickEcoNewsLink()
-//				.getNews().get(0).click().createAndPublicComment("Created comment")
-//				.getCommentByIndex(0).createOneReplyToComment("Reply comment") ;
-//		
-//		
-//		try {
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		//.clickCreateNewsButton().getHeader().clickEcoNewsLink()	;
-//		//ecoNews.createNews();
-//		//HomePage homePage = ecoNews.getHeader().logout();
-//	}
-
+ 
 	@Test
 	public void verifyLikeDislikeComment() {
 		
