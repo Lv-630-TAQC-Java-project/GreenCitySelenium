94b13507671d0e9024de7db98093460e1f67981d
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-11-03 12:09:02 +0200

    loadAllEcoNews

--- src/main/java/com/ss/ita/greencity/ui/pages/econews/EcoNewsListComponent.java
+++ src/main/java/com/ss/ita/greencity/ui/pages/econews/EcoNewsListComponent.java
@@ -35,12 +35,10 @@ public class EcoNewsListComponent extends BasePage {
     	return news.size();
     }
 
-
-
     public EcoNewsListComponent loadAllEcoNews() {
         WebDriverWait wait = new WebDriverWait(driver, 1);
         Actions scrollDown = new Actions(driver);
-        while(true){
+        while (true) {
             scrollDown.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
             try {
                 wait.until(ExpectedConditions.visibilityOfElementLocated(EcoNewsListLocators.LOADING_CIRCLE.getPath()));
@@ -49,15 +47,18 @@ public class EcoNewsListComponent extends BasePage {
             }
         }
         pause(2000);
-        System.out.println(new EcoNewsListComponent(driver).sizeEcoNewsListComponent());
         for (int i = 0; i < 5; i++) {
             scrollDown.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
-            System.out.println(i + " :" + pause(5000));
+            pause(1000);
         }
         pause(2000);
-        System.out.println(new EcoNewsListComponent(driver).sizeEcoNewsListComponent());
         return new EcoNewsListComponent(driver);
     }
-    
-    
-}
+
+    public int countNewsFromLabel() {
+        int count = 0;
+        String textLabel = driver.findElement(By.xpath("//div[@class = 'main-wrapper']//h2")).getText();
+        count = Integer.parseInt(textLabel.replaceAll("[^0123456789]", ""));
+        return count;
+    }
+ }
--- src/test/java/com/ss/ita/econews/ui/econews/AllEcoNewsTest.java
+++ src/test/java/com/ss/ita/econews/ui/econews/AllEcoNewsTest.java
@@ -4,6 +4,7 @@ import com.ss.ita.econews.ui.runner.TestRuner;
 import com.ss.ita.greencity.ui.pages.HomePage;
 import com.ss.ita.greencity.ui.pages.econews.EcoNewsListComponent;
 import com.ss.ita.greencity.ui.pages.econews.EcoNewsPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AllEcoNewsTest extends TestRuner {
@@ -11,8 +12,8 @@ public class AllEcoNewsTest extends TestRuner {
     @Test
     public void verifyLoadAllNews() {
         EcoNewsListComponent ecoNews = new HomePage(driver).getHeader().clickEcoNewsLink().getNews().loadAllEcoNews();
-        System.out.println("first pause in test" + " :" + ecoNews.pause(2000));
-        System.out.println(ecoNews.sizeEcoNewsListComponent());
-        System.out.println("second pause in test" + " :" + ecoNews.pause(5000));
+        int expectedCountNews = ecoNews.countNewsFromLabel();
+        int actualCountNews = ecoNews.sizeEcoNewsListComponent();
+        Assert.assertEquals(actualCountNews, expectedCountNews );
     }
 }
